d0c8724c8a7e6011a0bf230f139b88d2d82743ed
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-11-05 01:30:34 +0100

    Fix vanilla forge compat

--- Forge/src/main/java/com/teamacronymcoders/packmode/condition/minecraft/MinecraftCompat.java
+++ Forge/src/main/java/com/teamacronymcoders/packmode/condition/minecraft/MinecraftCompat.java
@@ -4,7 +4,7 @@ import com.teamacronymcoders.packmode.compat.Compat;
 import com.teamacronymcoders.packmode.condition.minecraft.condition.PackModeConditionSerializer;
 import net.minecraftforge.common.crafting.CraftingHelper;
 
-public class MineCraftCompat extends Compat {
+public class MinecraftCompat extends Compat {
     @Override
     public void setup() {
         CraftingHelper.register(new PackModeConditionSerializer());
