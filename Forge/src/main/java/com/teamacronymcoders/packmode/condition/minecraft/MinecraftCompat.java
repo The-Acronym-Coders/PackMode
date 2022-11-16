9f6b3b6a34b609396cbc5e6519cc3cc00dc7cc6d
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-11-17 00:10:38 +0100

    Move away from EBS, explain in / for datagen

--- Forge/src/main/java/com/teamacronymcoders/packmode/condition/minecraft/MinecraftCompat.java
+++ Forge/src/main/java/com/teamacronymcoders/packmode/condition/minecraft/MinecraftCompat.java
@@ -1,27 +1,15 @@
 package com.teamacronymcoders.packmode.condition.minecraft;
 
-import com.teamacronymcoders.packmode.PackModeConstants;
 import com.teamacronymcoders.packmode.compat.Compat;
 import com.teamacronymcoders.packmode.condition.minecraft.condition.PackModeConditionSerializer;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.common.crafting.CraftingHelper;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = PackModeConstants.MODID)
 public class MinecraftCompat extends Compat {
 
     @Override
     public void setup() {
-        //Setup is called later than we'd like, so EBS it is
-    }
-
-    @SubscribeEvent
-    public static void onRegisterSerializersEvent(RegistryEvent.Register<RecipeSerializer<?>> event) {
         CraftingHelper.register(new PackModeConditionSerializer());
     }
+
 }
