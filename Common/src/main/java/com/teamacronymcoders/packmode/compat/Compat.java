13974f316a702d872f74ed811a74d8f6bced8b53
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-11-28 17:22:16 +0100

    Add small javadoc

--- Common/src/main/java/com/teamacronymcoders/packmode/compat/Compat.java
+++ Common/src/main/java/com/teamacronymcoders/packmode/compat/Compat.java
@@ -1,5 +1,13 @@
 package com.teamacronymcoders.packmode.compat;
 
+/**
+ * <p>
+ * Register using {@link CompatHandler#registerCompat(String, Class)}
+ * </p>
+ *
+ *
+ * Extending classes MUST have a public, no argument constructor
+ */
 public abstract class Compat {
     public abstract void setup();
 }
